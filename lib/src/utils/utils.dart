84bc984da707a696c9d112109e5132912fe039f2
wheremyfiji <wher3myfiji@yandex.ru>
2023-04-28 16:54:05 +0300

    add error snackbar

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -18,11 +18,39 @@ void showSnackBar({
   var colors = Theme.of(ctx).colorScheme;
   var sm = ScaffoldMessenger.of(ctx);
   SnackBar snackBar = SnackBar(
-    content: Text(msg, style: TextStyle(color: colors.onSurfaceVariant)),
+    content: Text(
+      msg,
+      style: TextStyle(
+        color: colors.onSurfaceVariant,
+      ),
+    ),
     behavior: SnackBarBehavior.floating,
     backgroundColor: colors.surfaceVariant,
     dismissDirection: DismissDirection.horizontal,
     duration: dur ?? const Duration(seconds: 1),
+  );
+  sm.clearSnackBars();
+  sm.showSnackBar(snackBar);
+}
+
+void showErrorSnackBar({
+  required BuildContext ctx,
+  required String msg,
+  Duration? dur,
+}) {
+  var colors = Theme.of(ctx).colorScheme;
+  var sm = ScaffoldMessenger.of(ctx);
+  SnackBar snackBar = SnackBar(
+    content: Text(
+      msg,
+      style: TextStyle(
+        color: colors.onBackground,
+      ),
+    ),
+    behavior: SnackBarBehavior.floating,
+    backgroundColor: colors.errorContainer,
+    dismissDirection: DismissDirection.horizontal,
+    duration: dur ?? const Duration(seconds: 3),
   );
   sm.clearSnackBars();
   sm.showSnackBar(snackBar);
