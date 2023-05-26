e803ab797b2e1a6786ff588deaccf2a8e3c06e83
wheremyfiji <wher3myfiji@yandex.ru>
2023-05-26 12:25:28 +0300

    Update video_widget.dart

--- lib/src/presentation/pages/player/mobile/video_widget.dart
+++ lib/src/presentation/pages/player/mobile/video_widget.dart
@@ -28,79 +28,81 @@ class VideoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: FittedBox(
-        fit: expandVideo ? BoxFit.fitWidth : BoxFit.contain, //fitWidth  contain
-        child: SizedBox(
-          //aspectRatio: aspectRatio,
-          width: playerController.value.size.width,
-          height: playerController.value.size.height,
-          child: AspectRatio(
-            aspectRatio: aspectRatio,
-            child: DoubleTapPlayerView(
-              doubleTapConfig: DoubleTapConfig.create(
-                backDrop: Colors.black54,
-                fadeTime: switchDuration,
-                labelBuilder: (lr, tapCount) {
-                  String text = '';
+    return DoubleTapPlayerView(
+      doubleTapConfig: DoubleTapConfig.create(
+        backDrop: Colors.black54,
+        ovalColor: Colors.transparent,
+        rippleColor: Colors.transparent,
+        fadeTime: switchDuration,
+        backDropAnimDuration: Duration.zero,
+        expansionHoldingTime: Duration.zero,
+        labelBuilder: (lr, tapCount) {
+          String text = '';
 
-                  switch (lr) {
-                    case Lr.LEFT:
-                      text = '-10 сек.';
-                      break;
-                    case Lr.RIGHT:
-                      text = '+10 сек.';
-                      break;
-                    default:
-                  }
+          switch (lr) {
+            case Lr.LEFT:
+              text = '-10 сек.';
+              break;
+            case Lr.RIGHT:
+              text = '+10 сек.';
+              break;
+            default:
+          }
 
-                  return text;
-                },
-                onDoubleTap: (lr) {
-                  switch (lr) {
-                    case Lr.LEFT:
-                      onBack();
-                      break;
-                    case Lr.RIGHT:
-                      onForward();
-                      break;
-                    default:
-                  }
-                },
+          return text;
+        },
+        onDoubleTap: (lr) {
+          switch (lr) {
+            case Lr.LEFT:
+              onBack();
+              break;
+            case Lr.RIGHT:
+              onForward();
+              break;
+            default:
+          }
+        },
+      ),
+      swipeConfig: enableSwipe
+          ? SwipeConfig.create(
+              backDrop: Colors.black54,
+              overlayBuilder: (data) => CustomSwipeOverlay(
+                data: data,
+                currentPos: currentPosition,
               ),
-              swipeConfig: enableSwipe
-                  ? SwipeConfig.create(
-                      backDrop: Colors.black54,
-                      overlayBuilder: (data) => CustomSwipeOverlay(
-                        data: data,
-                        currentPos: currentPosition,
-                      ),
-                      onSwipeStart: (dx) {
-                        //debugPrint('swipe onSwipeStart: $dx');
-                        playerController.pause();
-                      },
-                      onSwipeCancel: () {
-                        //debugPrint('swipe onSwipeCancel');
-                        playerController.play();
-                      },
-                      onSwipeEnd: (data) async {
-                        if (data == null) {
-                          return;
-                        }
-                        //debugPrint(
-                        //    'swipe onSwipeEnd: ${data.startDx} | ${data.currentDx}');
+              onSwipeStart: (dx) {
+                //debugPrint('swipe onSwipeStart: $dx');
+                playerController.pause();
+              },
+              onSwipeCancel: () {
+                //debugPrint('swipe onSwipeCancel');
+                playerController.play();
+              },
+              onSwipeEnd: (data) async {
+                if (data == null) {
+                  return;
+                }
+                //debugPrint(
+                //    'swipe onSwipeEnd: ${data.startDx} | ${data.currentDx}');
 
-                        await playerController.seekTo(
-                          playerController.value.position +
-                              Duration(
-                                seconds:
-                                    (data.currentDx - data.startDx).toInt(),
-                              ),
-                        );
-                        await playerController.play();
-                      },
-                    )
-                  : null,
+                await playerController.seekTo(
+                  playerController.value.position +
+                      Duration(
+                        seconds: (data.currentDx - data.startDx).toInt(),
+                      ),
+                );
+                await playerController.play();
+              },
+            )
+          : null,
+      child: SizedBox.expand(
+        child: FittedBox(
+          fit: expandVideo ? BoxFit.fitWidth : BoxFit.contain,
+          child: SizedBox(
+            width: playerController.value.size.width,
+            height: playerController.value.size.height,
+            child: AspectRatio(
+              aspectRatio: aspectRatio,
               child: VideoPlayer(
                 playerController,
               ),
