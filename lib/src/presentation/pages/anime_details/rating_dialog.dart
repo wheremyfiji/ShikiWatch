9042855ec06f1ff7869281d0b0eddb871a1643f8
wheremyfiji <wher3myfiji@yandex.ru>
2024-01-27 18:42:42 +0300

    Update rating_dialog.dart

--- lib/src/presentation/pages/anime_details/rating_dialog.dart
+++ lib/src/presentation/pages/anime_details/rating_dialog.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
 
+import '../../../utils/extensions/buildcontext.dart';
+
 class RatingDialog extends StatelessWidget {
   const RatingDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      icon: const Icon(Icons.warning_rounded),
       title: const Text('Подтверждение возраста'),
-      content: const Text(
-          'Для продолжения необходимо подтвердить, что тебе уже исполнилось 18 лет'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          RichText(
+            text: TextSpan(
+              text: 'Данный тайтл имеет возрастное ограничение ',
+              style: context.textTheme.bodyMedium,
+              children: [
+                TextSpan(
+                  text: '18+',
+                  style: TextStyle(
+                    color: context.colorScheme.error,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          RichText(
+            text: TextSpan(
+              text:
+                  '\nДля продолжения необходимо подтвердить, что тебе уже исполнилось ',
+              style: context.textTheme.bodyMedium,
+              children: [
+                TextSpan(
+                  text: '18 лет',
+                  style: TextStyle(
+                    color: context.colorScheme.error,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // const Text(
+          //   'Для продолжения необходимо подтвердить, что тебе уже исполнилось 18 лет',
+          // ),
+        ],
+      ),
       actions: [
         TextButton(
           onPressed: () => Navigator.pop(context, false),
@@ -16,7 +56,7 @@ class RatingDialog extends StatelessWidget {
         ),
         FilledButton(
           onPressed: () => Navigator.pop(context, true),
-          child: const Text('Подтвердить'),
+          child: const Text('Мне есть 18 лет'),
         ),
       ],
     );
