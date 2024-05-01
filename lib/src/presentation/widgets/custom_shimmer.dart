bf5aa86f594b99e608dddefaf8fde84b82106c86
wheremyfiji <wher3myfiji@yandex.ru>
2024-05-01 18:00:22 +0300

    Update custom_shimmer.dart

--- lib/src/presentation/widgets/custom_shimmer.dart
+++ lib/src/presentation/widgets/custom_shimmer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:tinycolor2/tinycolor2.dart';
 import 'package:shimmer/shimmer.dart';
 
 import '../../utils/extensions/buildcontext.dart';
@@ -10,26 +9,20 @@ class CustomShimmer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final baseColor = context.isLightThemed
-        ? context.colorScheme.background.desaturate(50).darken(5)
-        : context.colorScheme.surfaceVariant;
-    final highlightColor = context.isLightThemed
-        ? context.colorScheme.background.desaturate(50).lighten(5)
-        : context.colorScheme.surfaceVariant.lighten(5);
+    // final baseColor = context.isLightThemed
+    //     ? context.colorScheme.background.desaturate(50).darken(5)
+    //     : context.colorScheme.surfaceVariant;
+    // final highlightColor = context.isLightThemed
+    //     ? context.colorScheme.background.desaturate(50).lighten(5)
+    //     : context.colorScheme.surfaceVariant.lighten(5);
+
+    final baseColor = context.colorScheme.surfaceVariant;
+    final hsl = HSLColor.fromColor(baseColor);
+    final l = hsl.lightness;
+    final highlightColor =
+        hsl.withLightness(l < 0.5 ? l + 0.1 : l - 0.1).toColor();
 
     return Shimmer.fromColors(
-      // gradient: LinearGradient(
-      //   begin: Alignment.centerLeft,
-      //   end: Alignment.centerRight,
-      //   colors: <Color>[
-      //     baseColor,
-      //     baseColor,
-      //     highlightColor,
-      //     baseColor,
-      //     baseColor
-      //   ],
-      //   stops: const <double>[0.0, 0.35, 0.5, 0.65, 1.0],
-      // ),
       baseColor: baseColor,
       highlightColor: highlightColor,
       child: Container(
@@ -37,14 +30,5 @@ class CustomShimmer extends StatelessWidget {
         child: const SizedBox.expand(),
       ),
     );
-
-    // return Shimmer.fromColors(
-    //   //baseColor: Theme.of(context).colorScheme.surface,
-    //   baseColor: Theme.of(context).colorScheme.surfaceVariant,
-    //   highlightColor: Theme.of(context).colorScheme.onInverseSurface,
-    //   child: Container(
-    //     color: Colors.black,
-    //   ),
-    // );
   }
 }
